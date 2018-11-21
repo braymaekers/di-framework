01864852f7dd22a33b08e9be5694ccc0b432f154
Beppe Raymaekers <beppe.raymaekers@gmail.com>
2018-11-21 12:07:09 +0100

    data_ingestion project added

---
+++ etl/data_ingestion/file-mgmt/ddl/dataset_oc_eur_chl_multi_l3_chl_1km_daily_rt_v02.sql
@@ -0,0 +1,2 @@
+CREATE TABLE sales_dwh.dataset_oc_eur_chl_multi_l3_chl_1km_daily_rt_v02 (
+period int, CHL_nearest numeric, CHL_idw numeric, lat numeric, lon numeric)
---
+++ etl/data_ingestion/file-mgmt/ddl/sv04_med_ingv_tem_an_fc_d.sql
@@ -0,0 +1,2 @@
+CREATE TABLE sales_dwh.sv04_med_ingv_tem_an_fc_d (
+period int, thetao_nearest numeric, thetao_idw numeric, lat numeric, lon numeric)
