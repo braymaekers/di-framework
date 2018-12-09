eeb037cef0106b71a0aaa913fe7d2c08a83eda8a
Beppe Raymaekers <beppe.raymaekers@gmail.com>
2018-12-09 19:42:00 +0100

    init.sh

--- configuration/bin/init.sh
+++ configuration/bin/init.sh
@@ -7,7 +7,8 @@ PENTAHO_ENV="${PENTAHO_ENV:-config-pdi-local}"
 echo Running with "$PENTAHO_ENV" environment settings
 
 # Load Environment Configuration
-. "$ROOT_DIR/configuration/$PENTAHO_ENV/env.conf"
+. $ROOT_DIR/configuration/$PENTAHO_ENV/env.conf
+echo PENTAHO_HOME = $PENTAHO_HOME
 
 # Set Additional Variables
 export KETTLE_CLIENT_DIR="$PENTAHO_HOME"/design-tools/data-integration
