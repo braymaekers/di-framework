0bc7f3cd067cc1099c58443372eba9e89ed359a8
Beppe Raymaekers <beppe.raymaekers@gmail.com>
2018-12-09 19:46:51 +0100

    init.sh

--- configuration/bin/init.sh
+++ configuration/bin/init.sh
@@ -11,7 +11,7 @@ echo Running with "$PENTAHO_ENV" environment settings
 echo PENTAHO_HOME = $PENTAHO_HOME
 
 # Set Additional Variables
-export KETTLE_CLIENT_DIR="$PENTAHO_HOME"/design-tools/data-integration
+export KETTLE_CLIENT_DIR=$PENTAHO_HOME/design-tools/data-integration
 export KETTLE_HOME=$ROOT_DIR/configuration/$PENTAHO_ENV
 export KETTLE_META_HOME=$ROOT_DIR/configuration/$PENTAHO_ENV
 export OPT="$OPT -DPENTAHO_METASTORE_FOLDER=$KETTLE_META_HOME"
